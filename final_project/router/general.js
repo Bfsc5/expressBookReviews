6944b02fff3ea9d93f8a83f8d3ee7c4cc5187cd0
Bfsc5 <stevencruz5@hotmail.com>
2026-02-14 12:30:34 +0000

    Update general.js

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -12,8 +12,8 @@ public_users.post("/register", (req,res) => {
 
 // Get the book list available in the shop
 public_users.get('/',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const book = req.params.book;  return res.status(300).json({message: "Yet to be implemented"});
+
 });
 
 // Get book details based on ISBN
