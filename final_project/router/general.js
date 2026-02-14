7539cbd007f9a89e3784435144d8eade33861208
Bfsc5 <stevencruz5@hotmail.com>
2026-02-14 13:17:29 +0000

    Update general.js

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -18,10 +18,16 @@ return res.status(200).send(JSON.stringify(books, null, 2));});
 });
 
 // Get book details based on ISBN
-public_users.get('/isbn/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
- });
+public_users.get('/isbn/:isbn', function (req, res) {
+    const isbn = req.params.isbn;      // 1. Get ISBN from URL
+    const book = books[isbn];          // 2. Look up the book by key
+
+    if (book) {
+        return res.status(200).send(JSON.stringify(book, null, 2));
+        return res.status(404).json({ message: "Book not found" });
+    }
+});  
+
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
